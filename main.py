dd8a2ec45840f8fdd3a72eefaca7dfdd14559dde
Jonathan Vollebregt <jonathan-vola@hotmail.com>
2014-03-10 22:13:38 +0100

    Workaround for keyboard interrupts

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
+import signal
 from gi.repository import Gtk
 from mainwindow import MainWindow
-from subcommand import CommandBuilder
 
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 win = MainWindow()
-win.bind_builder(CommandBuilder())
 win.show()
 Gtk.main()
-
-""" Apg bugs to work around:
-* Runs infinitely given impossible options - seems to be fixed with a hack in
-    APG itself that sets all characters to 'a' when this happens.
-* Space in seed value prints help file (hacked around it - stripping spaces on focus out)
-* APG doesn't generate spaces so it presumes a space is there by accident and
-    messes up the command if spaces are the only thing in the exclusion (Same hack)"""
