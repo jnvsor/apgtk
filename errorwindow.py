5cb857b0622469d1f5599ddf64ad2bcea6cf958e
Jonathan Vollebregt <jonathan-vola@hotmail.com>
2014-08-27 14:09:39 +0200

    Fix bug in error dialogue
    Seems like GTK changed something that broke the error dialogue

--- errorwindow.py
+++ errorwindow.py
@@ -2,7 +2,7 @@ from gi.repository import Gtk
 
 class ErrorDialogue(Gtk.MessageDialog):
     def __init__(self, message, details=None):
-        Gtk.MessageDialog.__init__(self,0,Gtk.DialogFlags.MODAL,Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, message, title="An error occurred!")
+        Gtk.MessageDialog.__init__(self,None,Gtk.DialogFlags.MODAL,Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, message, title="An error occurred!")
         
         if details:
             self.format_secondary_text(details)
